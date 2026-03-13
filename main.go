95f7d63950aeb9ff7c20f7061a022e56777c9eee
Ariel Zvinowanda <kazan@outlook.ph>
2026-03-13 18:41:19 +0200

    im doing stuff

--- main.go
+++ main.go
@@ -1,26 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 const version float64 = 1.0
 const (
-	ColorFound = "\x1b[31m"
+	ColorFound = "\x1b[32m"
 	ColorReset = "\x1b[0m"
 )
 
-func fileExits(fileName string) {
+type Commands struct {
+	CommandExit     string
+	CommandHelp     string
+	CommandVersion  string
+	CommandPrevious string
+}
+
+func NewCommands() *Commands {
+	return &Commands{
+		CommandExit:     "{exit",
+		CommandHelp:     "{help",
+		CommandVersion:  "{ver",
+		CommandPrevious: "{pre",
+	}
+}
+
+func fileExists(fileName string) {
 	info, err := os.Stat(fileName)
 	if errors.Is(err, os.ErrNotExist) {
 		return
 	}
-	fmt.Printf("File Found: %v%s%v", ColorFound, info.Name(), ColorReset)
+	fmt.Printf("File Found: %v%s%v\n", ColorFound, info.Name(), ColorReset)
 }
 
 func main() {
-	fmt.Printf("Go-Grep v[%.1f]", version)
-	fileExits("main.go")
+	progIsRunning := true
+	scanner := bufio.NewScanner(os.Stdin)
+	cmd := NewCommands()
+	re := regexp.MustCompile(`^[a-zA-Z0-9 _-]+$`)
+	fmt.Printf("go_grep v[%.1f]\n", version)
+
+	for progIsRunning {
+		fmt.Print("> ")
+		scanner.Scan()
+
+		switch scanner.Text() {
+		case cmd.CommandExit:
+			fmt.Println("Exiting...")
+			progIsRunning = false
+		case cmd.CommandHelp:
+			fmt.Print("Commands: {help, {exit, {ver\n")
+		case cmd.CommandVersion:
+			fmt.Printf("go_grep v[%.1f]\n", version)
+
+		default:
+			if re.MatchString(scanner.Text()) {
+				fileExists(scanner.Text())
+			}
+		}
+	}
 }
