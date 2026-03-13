1bf2fe6ea03f5f6e687ef607fb14affd0747a187
Ariel Zvinowanda <kazan@outlook.ph>
2026-03-13 17:35:48 +0200

    changes ngl

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 const version float64 = 1.0
+const (
+	ColorFound = "\x1b[31m"
+	ColorReset = "\x1b[0m"
+)
+
+func fileExits(fileName string) {
+	info, err := os.Stat(fileName)
+	if errors.Is(err, os.ErrNotExist) {
+		return
+	}
+	fmt.Printf("File Found: %v%s%v", ColorFound, info.Name(), ColorReset)
+}
 
 func main() {
 	fmt.Printf("Go-Grep v[%.1f]", version)
+	fileExits("main.go")
 }
